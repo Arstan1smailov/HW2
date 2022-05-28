feb2242475f8c0e219c92d0bc5a3705421c77d62
Arstan1smailov <drkskillet@gmail.com>
2022-05-29 00:56:33 +0600

    Initial commit

---
+++ app/src/main/java/com/arstan/myapplication/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.arstan.myapplication
+
+import android.graphics.ImageDecoder
+import android.media.Image
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.webkit.URLUtil
+import android.widget.Toast
+import com.arstan.myapplication.databinding.ActivityMainBinding
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.ImageHeaderParser
+import java.io.BufferedReader
+import java.net.URI
+import java.net.URLConnection
+import java.net.URLDecoder
+import java.net.URLEncoder
+import java.sql.Connection
+import java.util.*
+import javax.xml.transform.Source
+import kotlin.collections.ArrayList
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var listURI: ArrayList<String> = ArrayList()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        shit()
+
+        binding.btnSubmit.setOnClickListener {
+
+            val url = Uri.parse(binding.etImageLink.text.toString())
+
+            if ( URLUtil.isValidUrl(url.toString())) {
+
+                Glide.with(this)
+                    .load(url)
+                    .into(binding.img)
+                listURI.add(binding.etImageLink.text.toString())
+                Toast.makeText(this, getString(R.string.success), Toast.LENGTH_SHORT).show()
+
+            } else {
+
+                Toast.makeText(this, getString(R.string.valid_url), Toast.LENGTH_SHORT).show()
+
+            }
+        }
+
+        binding.randomImage.setOnClickListener {
+
+            val random = (0 until listURI.size - 1).random()
+
+            Glide.with(this)
+                .load(Uri.parse(listURI[random]))
+                .into(binding.img)
+
+        }
+    }
+    private fun shit (){
+        listURI.add("https://images.unsplash.com/photo-1566275529824-cca6d008f3da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cGhvdG98ZW58MHx8MHx8&w=1000&q=80")
+        listURI.add("https://images.unsplash.com/photo-1566275529824-cca6d008f3da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cGhvdG98ZW58MHx8MHx8&w=1000&q=80")
+        listURI.add("https://images.unsplash.com/photo-1566275529824-cca6d008f3da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cGhvdG98ZW58MHx8MHx8&w=1000&q=80")
+        listURI.add("https://images.unsplash.com/photo-1566275529824-cca6d008f3da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cGhvdG98ZW58MHx8MHx8&w=1000&q=80")
+        listURI.add("https://images.unsplash.com/photo-1566275529824-cca6d008f3da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cGhvdG98ZW58MHx8MHx8&w=1000&q=80")
+        listURI.add("https://images.unsplash.com/photo-1566275529824-cca6d008f3da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cGhvdG98ZW58MHx8MHx8&w=1000&q=80")
+        listURI.add("https://images.unsplash.com/photo-1566275529824-cca6d008f3da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cGhvdG98ZW58MHx8MHx8&w=1000&q=80")
+        listURI.add("https://images.unsplash.com/photo-1566275529824-cca6d008f3da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cGhvdG98ZW58MHx8MHx8&w=1000&q=80")
+        listURI.add("https://images.unsplash.com/photo-1566275529824-cca6d008f3da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cGhvdG98ZW58MHx8MHx8&w=1000&q=80")
+        listURI.add("https://images.unsplash.com/photo-1566275529824-cca6d008f3da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cGhvdG98ZW58MHx8MHx8&w=1000&q=80")
+        listURI.add("https://images.unsplash.com/photo-1566275529824-cca6d008f3da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cGhvdG98ZW58MHx8MHx8&w=1000&q=80")
+        listURI.add("https://images.unsplash.com/photo-1566275529824-cca6d008f3da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cGhvdG98ZW58MHx8MHx8&w=1000&q=80")
+        listURI.add("https://images.unsplash.com/photo-1566275529824-cca6d008f3da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cGhvdG98ZW58MHx8MHx8&w=1000&q=80")
+        listURI.add("https://images.unsplash.com/photo-1566275529824-cca6d008f3da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cGhvdG98ZW58MHx8MHx8&w=1000&q=80")
+        listURI.add("https://images.unsplash.com/photo-1566275529824-cca6d008f3da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cGhvdG98ZW58MHx8MHx8&w=1000&q=80")
+         }
+
+}
